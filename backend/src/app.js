f34857c4314cde441e1861c241c79fbca1072ffd
Shahin Mia <shahin01777296933@gmail.com>
2023-08-14 23:37:25 +0600

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,23 +6,12 @@ const bodyParser = require("body-parser");
 const orderRouter = require("./routers/orderRouter");
 
 const cors = require("cors");
-
-// var corsOptions = {
-//   origin: "https://deploy-mern-frontend-five.vercel.app",
-//   optionsSuccessStatus: 200,
-//   methods: [ "GET, PUT, POST, DELETE"],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+ app.use(cors())
+// app.use(cors({
+//   origin: ['https://deploy-mern-frontend-five.vercel.app'],
+//   methods: [ "GET, POST,],
 //   credentials: true,
-// };
-
-app.use(cors({
-  origin: ['https://deploy-mern-frontend-five.vercel.app'],
-  methods: [ "GET, POST,],
-  credentials: true,
-}));
-
-//deploy-mern-frontend-five.vercel.app
+// }));
 
 // app.use(function (req, res, next) {
 //   res.header('Access-Control-Allow-Origin', 'https://deploy-mern-frontend-five.vercel.app');
