3f5f8c5c2476b9aafb87e839e10e5ff92e2ecb84
Shahin Mia <shahin01777296933@gmail.com>
2023-08-14 21:51:16 +0600

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,26 +5,26 @@ const app = express();
 const bodyParser = require("body-parser");
 const orderRouter = require("./routers/orderRouter");
 
-const cors = require("cors");
+// const cors = require("cors");
 
 // var corsOptions = {
 //   origin: "https://deploy-mern-frontend-five.vercel.app",
 //   optionsSuccessStatus: 200,
 // };
 
-app.use(cors());
+// app.use(cors());
 
 //deploy-mern-frontend-five.vercel.app
 
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://deploy-mern-frontend-five.vercel.app");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://deploy-mern-frontend-five.vercel.app');
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
