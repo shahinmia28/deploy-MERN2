a4561b6f32ab89cab47caeec1a29a50fbe25adf9
Shahin Mia <shahin01777296933@gmail.com>
2023-08-14 21:25:55 +0600

    Update app.js 14

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,15 +14,15 @@ app.use(cors(corsOptions));
 
 //deploy-mern-frontend-five.vercel.app
 
-// https: app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://deploy-mern-frontend-five.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
